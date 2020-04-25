c82869c58a7190d272d967de3286245f5f44644c
OmardTech <omardiaz2012@gmail.com>
2020-04-25 16:58:43 -0700

    Added Deck Class

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 playing = True
 
-for suits in suits:
-    for ranks in ranks:
-        print (suits)
 
 
 class Card:
@@ -26,3 +23,23 @@ class Card:
 
 
 
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))  # build Card objects and add them to the list
+    
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
